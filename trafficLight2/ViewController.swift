4751208149f4109ab406c41783089df291d4d3a3
Roman <roman.d.kozlov@gmail.com>
2022-02-05 00:35:46 +0300

    two variants

--- trafficLight2/ViewController.swift
+++ trafficLight2/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+//    var activeLight = ActiveLight.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setStartAlpha()
+        setCornerRadius()
     }
 
-
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        
+        if redLight.alpha == 0.3 {
+            redLight.alpha = 1
+        }
+//        setStartAlpha()
+//        if greenLight.alpha == 0.3 && yellowLight.alpha == 0.3 {
+//            redLight.alpha = 1
+//        } else if redLight.alpha == 1 {
+//            redLight.alpha = 0.3
+//            yellowLight.alpha = 1
+//        } else if yellowLight.alpha == 1 {
+//            yellowLight.alpha = 0.3
+//            greenLight.alpha = 1
+//        }
+        
+//        switch activeLight {
+//        case .red:
+//            redLight.alpha = 1
+//            activeLight = .yellow
+//        case .yellow:
+//            yellowLight.alpha = 1
+//            activeLight = .green
+//        case .green:
+//            greenLight.alpha = 1
+//            activeLight = .red
+//        }
+    }
+    
+    func setStartAlpha() {
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+    }
+    
+    func setCornerRadius() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+        startButton.layer.cornerRadius = 15
+    }
+    
+//    enum ActiveLight {
+//        case red
+//        case yellow
+//        case green
+//    }
 }
 
