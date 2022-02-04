a8a4bb59b7bc4a7b0442e6442fe8cb7d6ed3af45
Roman <roman.d.kozlov@gmail.com>
2022-02-04 23:22:07 +0300

    Initial Commit

---
+++ trafficLight2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLight2
+//
+//  Created by Roman Kozlov on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
